17e99cdb09001e752bdd8227ffd06db0548ec9fc
Jean Santos <santosjeandx@gmail.com>
2021-11-15 17:00:47 -0500

    Obteniendo data de Firebase JSON

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,17 +8,41 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
+    //leer archivo JSON LOCAL
     this.http.get('assets/data/data-pagina.json')
-    .subscribe((resp: InfoPagina) => {
-      /* .subscribe((resp:any) => {
-        // console.log(resp)
-        console.log(resp.email);
-      }) */
-      this.cargada = true;
-      this.info = resp;
-      console.log(resp)
-    })
+      .subscribe((resp: InfoPagina) => {
+        /* .subscribe((resp:any) => {
+          // console.log(resp)
+          console.log(resp.email);
+        }) */
+        this.cargada = true;
+        this.info = resp;
+        console.log(resp)
+      })
+  }
+
+  private cargarEquipo() {
+    //leer archivo JSON DE FIREBASE
+    this.http.get('https://angular-html-1e165-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe((res: any) => {
+        this.equipo = res;
+        console.log(res)
+      })
+
+    /* fetch('https://angular-html-1e165-default-rtdb.firebaseio.com/equipo.json')
+    .then(res => res.json())
+    .then(data => {
+      data.forEach( (team:any) => {
+        console.log(team)
+      })
+    }) */
   }
 }
