49d66d430455d0d1a65147839e6b605a1cc42a72
Jean Santos <santosjeandx@gmail.com>
2021-11-15 22:21:05 -0500

    update lasted

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Producto } from '../interfaces/producto.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  cargando = true;
+  productos: Producto[] = [];
+
+  constructor(private http: HttpClient) {
+    this.cargarProductos();
+  }
+
+  private cargarProductos() {
+
+    this.http.get('https://angular-html-25cf9.firebaseio.com/productos_idx.json')
+      .subscribe((resp: any) => {
+        // console.log(resp);
+        this.productos = resp;
+        this.cargando = false;
+        /* setTimeout(() => {
+          this.cargando = false;
+        },1000); */
+      });
+  }
 }
